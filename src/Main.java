72e61cc585480acd8154ea2d4ab3bd0a789d9425
noasman <noah.kichenassamy@edu.esiee-it.fr>
2024-10-07 16:47:30 +0200

    TP1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] score = new int[7];
+        int num = 0;
+        int denom = 0;
+        for (int i = 0; i < score.length; i++) {
+            score[i] = i+5;
+            num+=score[i];
+            denom= score.length;
+        }
+
+        float moy = num/denom;
+        System.out.println(num);
+        System.out.println(denom);
+        System.out.println(moy);
+
+
+
+
+
         }
     }
-}
